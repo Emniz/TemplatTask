aeaaa5e30b28773503a1b4c6fb1aaf15057c3616
Reshetnikov Roman <romanokd@yandex.ru>
2020-04-08 22:37:32 +0300

    Check and merge stack

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -5,4 +5,13 @@ int main()
 {
     Stack<int> stack(5);
     Stack<int> stackSecond(3);
+    stack.push(2);
+    stack.push(4);
+    stack.push(6);
+    stack.push(8);
+    stack.push(11);
+    stackSecond.push(3);
+    stackSecond.push(5);
+    stack.pop();
+    stack.StackMerge(stackSecond);
 }
