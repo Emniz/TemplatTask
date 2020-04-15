4a156264578055f5ce78e9680569bb17a4f1a8a6
Reshetnikov Roman <romanokd@yandex.ru>
2020-04-16 00:39:10 +0300

    Make insert and Turn's

--- AVL/AVLtree.h
+++ AVL/AVLtree.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+
 template <class T>
 class AVLTree;
 
@@ -47,6 +48,81 @@ private:
 	}
 
 public:
-	friend class AvlTree<T>;
+	friend class AVLTree<T>;
 };
+template <class T>
+class AVLTree
+{
+private:
+	AVLNode<T>* root;
+	int count;
+	bool ch;
+	void Balance(AVLNode<T>*& index)
+	{
+		int OldHeight = index->height;
+		index->MakeHeight();
+		int balance = index->Balance();
+		if (balance > 1)
+		{
+			if (index->right->Balance() < 0)
+				this->TurnLeft(index->right);
+			this->TurnRight(index);
+			if (index->height == OldHeight)
+				this->ch = false;
+		}
+		else if (balance < -1)
+		{
+			if (index->left->Balance() > 0)
+				this->TurnRight(index->left);
+			this->TurnLeft(index);
+			if (index->height == OldHeight)
+				this->ch = false;
+		}
+	}
 
+	void Insert(AVLNode<T>*& index, T data)
+	{
+		if (index == nullptr)
+		{
+			this->ch = true;
+			index = new AVLNode<T>(data);
+		}
+		else
+		{
+			if (data < index->data)
+			{
+				this->Insert(index->left, data);
+				if (this->ch)
+					this->Balance(index);
+			}
+			else
+			{
+				this->Insert(index->right, data);
+				if (this->ch)
+					this->Balance(index);
+			}
+		}
+	}
+	void TurnLeft(AVLNode<T>*& index)
+	{
+		AVLNode<T>* temp;
+		temp = index->left;
+		index->left = temp->right;
+		temp->right = index;
+		index->MakeHeight();
+		temp->MakeHeight();
+		index = temp;
+	}
+
+	void TurnRight(AVLNode<T>*& index)
+	{
+		AVLNode<T>* temp;
+		temp = index->right;
+		index->right = temp->left;
+		temp->left = index;
+		index->MakeHeight();
+		temp->MakeHeight();
+		index = temp;
+	}
+public:
+};
